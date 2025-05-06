9a4689759f8eca3ce7e948cc6425d306cbac08d1
Yuyuwayu <simpersherta@gmail.com>
2025-05-06 17:13:59 +0700

    fix main.css

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/views/LoginView.vue'
+
+const routes = [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: LoginView },
+    { path: '/dashboard', component: () => import('@/views/DashboardView.vue') }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
